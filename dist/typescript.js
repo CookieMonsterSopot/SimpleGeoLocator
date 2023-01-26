1bd325810b07de0c920dc7cabf13f6c4c4b5751c
Grzegorz Wozniak <grubysweter@gmail.com>
2023-01-26 12:55:06 +0100

    sidebar API

---
+++ dist/typescript.js
@@ -0,0 +1,162 @@
+// 1. PODSTAWOWE TYPY DANYCH
+// a) number
+// const birthDate = 1990;
+// // let age: number;
+// let age = 30;
+// b) string
+let firstName = "John";
+const lastName = "Smith";
+// c) boolean (true/false)
+const bool = true;
+let x;
+// 2. TYPOWANIE FUNKCJI
+const multiply = (a, b) => {
+    return a * b;
+};
+const addAndLog = (a, b) => {
+    console.log(a + b);
+};
+// Zad 1.
+// Napisz funkcje która będzie obliczała cenę w zależności od tego czy zniżka jest przyznana czy nie
+// calculatePrice
+// Parametry: originalPrice (liczba), hasDiscount (true/false), hasDiscount stwórz jako osobną zmienną ustaw ją dowolnie na true lub false
+// Funkcjonalność: Jeżeli parametr hasDiscount jest równy true, zwróć liczbę originalPrice pomnijeszoną o 23%, jeżeli hasDiscount jest równe false, zwróć liczbę originalPrice bez żadnych modyfikacji.
+const hasDiscount = true;
+const calculatePrice = (originalPrice, hasDiscount) => {
+    if (hasDiscount)
+        return originalPrice / 1.23;
+    return originalPrice;
+};
+const result = calculatePrice(1000, hasDiscount);
+//console.log(result);
+// 3. TYPOWANIE ELEMENTÓW DOM
+const button = document.querySelector("button");
+const categoriesUL = document.querySelector(".categories");
+button.addEventListener("click", () => {
+    // 1. W jakiś sposób wylosuj czy użytkownik ma zniżke czy nie i zapisz do zmiennej true albo false
+    const hasDiscount = Math.random() > 0.5 ? true : false;
+    // 2. Ściągnij wartośc z inputu textowego (id name).
+    const amount = +document.querySelector("input").value;
+    // 3. Wykonaj console loga "You have to pay *tu cena ktora jest wynikiem wywołania calculatePrice*". Na 1 parametr calculatePrice podaj wartość z inputu, na 2 wartość z pkt 1.
+    console.log(`You have to pay ${calculatePrice(amount, hasDiscount)}`);
+});
+// 1. Napisz funkcję renderForm, w funkcji twórz i wyświetlaj (createElement, appendChild) form, w nim 4 inputy: imie, nazwisko, kod pocztowy i miejscowość, ulica i numer domu. Dodaj też button type submit.
+// 2. Do formularza podepnij event listener, przy submitcie formularza, wyświetl na ekranie h2 z tekstem 'Paczka wysłana, zamawiający: *imie, nazwisko, kod pocztowy, miejscowość, ulica, nr domu*'
+const renderForm = () => {
+    const form = document.createElement("form");
+    const firstNameInput = document.createElement("input");
+    const secondNameInput = document.createElement("input");
+    const codeInput = document.createElement("input");
+    const addressInput = document.createElement("input");
+    const submitButton = document.createElement("button");
+    submitButton.type = "Submit";
+    submitButton.textContent = "Submit";
+    //   firstNameInput.classList.add("form-input");
+    //   secondNameInput.classList.add("form-input");
+    //   codeInput.classList.add("form-input");
+    //   addressInput.classList.add("form-input");
+    firstNameInput.id = "form-input-1";
+    secondNameInput.id = "form-input-2";
+    codeInput.id = "form-input-3";
+    addressInput.id = "form-input-4";
+    form.appendChild(firstNameInput);
+    form.appendChild(secondNameInput);
+    form.appendChild(codeInput);
+    form.appendChild(addressInput);
+    form.appendChild(submitButton);
+    form.addEventListener("submit", (e) => {
+        console.log(123);
+        e.preventDefault();
+        const input1 = document.getElementById("form-input-1")
+            .value;
+        const input2 = document.getElementById("form-input-2")
+            .value;
+        const input3 = document.getElementById("form-input-3")
+            .value;
+        const input4 = document.getElementById("form-input-4")
+            .value;
+        // const inputs = [...document.getElementsByClassName("form-input")];
+        // const values = inputs.map((input: HTMLInputElement) => input.value);
+        console.log(`Paczka wysłana, zamawiający: ${input1} ${input2} ${input3} ${input4}`);
+    });
+    document.body.appendChild(form);
+};
+//renderForm();
+// 4. UNION TYPE
+let y = 50;
+y = 5;
+y = "alalalala";
+y = true;
+y = [1, 2, 2, 4, 4, 5, 6];
+//let year: TwentyTwenties = 2035
+// 7. TYPOWANIE OBIEKTÓW
+const john = {
+    firstName: "John",
+    lastName: "Smith",
+    age: 30,
+    married: true,
+    pet: "Dog",
+};
+// const peter: Programmer = {
+//   firstName: "Peter",
+//   lastName: "Jackson",
+//   age: 25,
+//   married: false,
+//   pet: "Cat",
+//   language: "Python",
+//   company: "Microsoft",
+// }
+// 8. Typowanie list
+const people = [
+    {
+        firstName: "John",
+        lastName: "Smith",
+        age: 30,
+        married: true,
+        pet: "Dog",
+    },
+    {
+        firstName: "Peter",
+        lastName: "Jackson",
+        age: 25,
+        married: false,
+        pet: "Cat",
+        language: "Python",
+        company: "Microsoft",
+    },
+    [1, 2, 3, 45, 56, 7],
+];
+// 1. Stwórz funkcję renderTodoList (async/await)
+// const renderTodoList = async () => {
+//   // 2. W funkcji renderTodoList strzel do https://jsonplaceholder.typicode.com/todos/ fetchem, pamiętaj o await.
+//   const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+//   // 3. Rozpakuj odpowiedź metodą JSON. Również pamiętaj o await.
+//   const datas = await response.json();
+//   // 4. Stwórz element ul (createElement()).
+//   type TodoObj = typeof datas[0];
+//   const ul = document.createElement("ul");
+//   // 5. Z każdego todosa pochodzącego z API wyciągnij własność title. Możesz skorzystać z metody .map(). Wszystkie tytuły składuj w liście w zmiennej titles.
+//   let titles = datas.map((todos: TodoObj) => todos.title);
+//   // 6. Dla każdego tytułu stwórz element li (createElement()), wykorzystując forEach. Ustaw textContent li na aktualny tytuł (1 paramter forEach). Zaraz po tym przyczep (appendChild) li do ul'a (pkt 4).
+//   titles.forEach((title: string) => {
+//     const li = document.createElement("li");
+//     li.textContent = title;
+//     ul.appendChild(li);
+//   });
+//   // 7. Przyczep (appendChild) ul'a do body strony.
+//   document.body.appendChild(ul);
+// };
+// renderTodoList();
+// const renderTodoList = async () => {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+//   const datas = await response.json();
+//   const ul = document.createElement("ul");
+//   let titles = datas.map((todos) => todos.title);
+//   titles.forEach((title) => {
+//     const li = document.createElement("li");
+//     li.textContent = title;
+//     ul.appendChild(li);
+//   });
+//   document.body.appendChild(ul);
+// };
+// renderTodoList();
--- src/app.ts
+++ src/app.ts
@@ -1,236 +1,48 @@
-// 1. PODSTAWOWE TYPY DANYCH
+let getWeather = function () {
+  if (navigator.geolocation) {
+    const options = {
+      enableHighAccuracy: true,
+    };
+    navigator.geolocation.getCurrentPosition(function (position) {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      console.log(lat, lon);
+      const url =
+        "https://api.openweathermap.org/data/2.5/weather?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&units=metric&appid=46079d357e9a2c65ff03fab0ab51f02c";
 
-// a) number
+      const renderTodoList = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        let cityNow = data.name;
 
-const birthDate = 1990;
-// // let age: number;
-// let age = 30;
+        let tempNow = data.main.temp;
+        console.log(cityNow);
+        console.log("Current temperature: " + tempNow);
 
-// b) string
-let firstName: string = "John";
-const lastName: string = "Smith";
+        const ul = document.createElement("ul");
+        const liCity = document.createElement("li");
+        const h1City = document.createElement("h1");
 
-// c) boolean (true/false)
-const bool: boolean = true;
-let x: boolean;
+        h1City.innerHTML = cityNow;
 
-// 2. TYPOWANIE FUNKCJI
-const multiply = (a: number, b: number): number => {
-  return a * b;
+        const liTemp = document.createElement("li");
+        liTemp.textContent = "Current temperature: " + tempNow + " ºC";
+        liCity.appendChild(h1City);
+        ul.appendChild(liCity);
+        ul.appendChild(liTemp);
+
+        document.body.appendChild(ul);
+      };
+      renderTodoList();
+    });
+  } else {
+    window.alert("Could not get location");
+  }
 };
 
-const addAndLog = (a: number, b: number): void => {
-  console.log(a + b);
-};
-
-// Zad 1.
-// Napisz funkcje która będzie obliczała cenę w zależności od tego czy zniżka jest przyznana czy nie
-// calculatePrice
-// Parametry: originalPrice (liczba), hasDiscount (true/false), hasDiscount stwórz jako osobną zmienną ustaw ją dowolnie na true lub false
-// Funkcjonalność: Jeżeli parametr hasDiscount jest równy true, zwróć liczbę originalPrice pomnijeszoną o 23%, jeżeli hasDiscount jest równe false, zwróć liczbę originalPrice bez żadnych modyfikacji.
-
-const hasDiscount = true;
-
-const calculatePrice = (originalPrice: number, hasDiscount: boolean) => {
-  if (hasDiscount) return originalPrice / 1.23;
-  return originalPrice;
-};
-
-const result = calculatePrice(1000, hasDiscount);
-//console.log(result);
-
-// 3. TYPOWANIE ELEMENTÓW DOM
-
-const button = document.querySelector("button");
-const categoriesUL: HTMLUListElement = document.querySelector(".categories");
-
-button.addEventListener("click", () => {
-  // 1. W jakiś sposób wylosuj czy użytkownik ma zniżke czy nie i zapisz do zmiennej true albo false
-  const hasDiscount = Math.random() > 0.5 ? true : false;
-  // 2. Ściągnij wartośc z inputu textowego (id name).
-  const amount = +document.querySelector("input").value;
-  // 3. Wykonaj console loga "You have to pay *tu cena ktora jest wynikiem wywołania calculatePrice*". Na 1 parametr calculatePrice podaj wartość z inputu, na 2 wartość z pkt 1.
-  console.log(`You have to pay ${calculatePrice(amount, hasDiscount)}`);
-});
-
-// 1. Napisz funkcję renderForm, w funkcji twórz i wyświetlaj (createElement, appendChild) form, w nim 4 inputy: imie, nazwisko, kod pocztowy i miejscowość, ulica i numer domu. Dodaj też button type submit.
-// 2. Do formularza podepnij event listener, przy submitcie formularza, wyświetl na ekranie h2 z tekstem 'Paczka wysłana, zamawiający: *imie, nazwisko, kod pocztowy, miejscowość, ulica, nr domu*'
-
-const renderForm = () => {
-  const form = document.createElement("form");
-  const firstNameInput = document.createElement("input");
-  const secondNameInput = document.createElement("input");
-  const codeInput = document.createElement("input");
-  const addressInput = document.createElement("input");
-  const submitButton = document.createElement("button");
-  submitButton.type = "Submit";
-  submitButton.textContent = "Submit";
-
-  //   firstNameInput.classList.add("form-input");
-  //   secondNameInput.classList.add("form-input");
-  //   codeInput.classList.add("form-input");
-  //   addressInput.classList.add("form-input");
-
-  firstNameInput.id = "form-input-1";
-  secondNameInput.id = "form-input-2";
-  codeInput.id = "form-input-3";
-  addressInput.id = "form-input-4";
-
-  form.appendChild(firstNameInput);
-  form.appendChild(secondNameInput);
-  form.appendChild(codeInput);
-  form.appendChild(addressInput);
-  form.appendChild(submitButton);
-
-  form.addEventListener("submit", (e: Event) => {
-    console.log(123);
-    e.preventDefault();
-    const input1 = (document.getElementById("form-input-1") as HTMLInputElement)
-      .value;
-    const input2 = (document.getElementById("form-input-2") as HTMLInputElement)
-      .value;
-    const input3 = (document.getElementById("form-input-3") as HTMLInputElement)
-      .value;
-    const input4 = (document.getElementById("form-input-4") as HTMLInputElement)
-      .value;
-    // const inputs = [...document.getElementsByClassName("form-input")];
-    // const values = inputs.map((input: HTMLInputElement) => input.value);
-    console.log(
-      `Paczka wysłana, zamawiający: ${input1} ${input2} ${input3} ${input4}`
-    );
-  });
-
-  document.body.appendChild(form);
-};
-//renderForm();
-
-// 4. UNION TYPE
-let y: YVariable = 50;
-y = 5;
-y = "alalalala";
-y = true;
-y = [1, 2, 2, 4, 4, 5, 6];
-//y = {}
-// y = true
-
-// 5. TYPE ALIAS
-type AnotherAlias = string;
-type YVariable = number | string | boolean | number[] | AnotherAlias;
-
-// 6. TYPE ALIAS + UNION TYPES: twarde ustawienie zestawu wartości
-type TwentyTwenties =
-  | 2020
-  | 2021
-  | 2022
-  | 2023
-  | 2024
-  | 2025
-  | 2026
-  | 2027
-  | 2028
-  | 2029;
-
-//let year: TwentyTwenties = 2035
-
-// 7. TYPOWANIE OBIEKTÓW
-
-const john: Person = {
-  firstName: "John",
-  lastName: "Smith",
-  age: 30,
-  married: true,
-  pet: "Dog",
-};
-// a) type alias
-// type Person = { firstName: string; lastName: string; age: number, married: boolean };
-
-// b) interface
-interface Person {
-  firstName: string;
-  lastName: string;
-  age: number;
-  married: boolean;
-}
-
-interface Person {
-  pet: string;
-}
-
-interface Programmer extends Person {
-  language: string;
-  company: string;
-}
-
-// const peter: Programmer = {
-//   firstName: "Peter",
-//   lastName: "Jackson",
-//   age: 25,
-//   married: false,
-//   pet: "Cat",
-//   language: "Python",
-//   company: "Microsoft",
-// }
-
-// 8. Typowanie list
-const people: (Person | Programmer | number[])[] = [
-  {
-    firstName: "John",
-    lastName: "Smith",
-    age: 30,
-    married: true,
-    pet: "Dog",
-  },
-  {
-    firstName: "Peter",
-    lastName: "Jackson",
-    age: 25,
-    married: false,
-    pet: "Cat",
-    language: "Python",
-    company: "Microsoft",
-  },
-  [1, 2, 3, 45, 56, 7],
-];
-
-interface TodoObj {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
-// 1. Stwórz funkcję renderTodoList (async/await)
-const renderTodoList = async () => {
-  // 2. W funkcji renderTodoList strzel do https://jsonplaceholder.typicode.com/todos/ fetchem, pamiętaj o await.
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
-  // 3. Rozpakuj odpowiedź metodą JSON. Również pamiętaj o await.
-  const datas = await response.json();
-  // 4. Stwórz element ul (createElement()).
-  type TodoObj = typeof datas[0];
-
-  const ul = document.createElement("ul");
-  // 5. Z każdego todosa pochodzącego z API wyciągnij własność title. Możesz skorzystać z metody .map(). Wszystkie tytuły składuj w liście w zmiennej titles.
-  let titles = datas.map((todos: TodoObj) => todos.title);
-  // 6. Dla każdego tytułu stwórz element li (createElement()), wykorzystując forEach. Ustaw textContent li na aktualny tytuł (1 paramter forEach). Zaraz po tym przyczep (appendChild) li do ul'a (pkt 4).
-  titles.forEach((title: string) => {
-    const li = document.createElement("li");
-    li.textContent = title;
-    ul.appendChild(li);
-  });
-  // 7. Przyczep (appendChild) ul'a do body strony.
-  document.body.appendChild(ul);
-};
-renderTodoList();
-
-// const renderTodoList = async () => {
-//   const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
-//   const datas = await response.json();
-//   const ul = document.createElement("ul");
-//   let titles = datas.map((todos) => todos.title);
-//   titles.forEach((title) => {
-//     const li = document.createElement("li");
-//     li.textContent = title;
-//     ul.appendChild(li);
-//   });
-//   document.body.appendChild(ul);
-// };
-// renderTodoList();
+getWeather();
