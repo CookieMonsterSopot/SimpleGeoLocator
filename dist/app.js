6c828f15f6565661455a6e348379a0024e24fbb5
Grzegorz Wozniak <grubysweter@gmail.com>
2023-01-26 23:50:22 +0100

    update better

--- dist/app.js
+++ dist/app.js
@@ -1,53 +1,82 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
+  };
 let getWeather = function () {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        let lat = position.coords.latitude;
-        let lon = position.coords.longitude;
-        console.log(lat, lon);
-        const url = "https://api.openweathermap.org/data/2.5/weather?lat=" +
-            lat +
-            "&lon=" +
-            lon +
-            "&units=metric&appid=46079d357e9a2c65ff03fab0ab51f02c";
-        const renderLocalList = () => __awaiter(this, void 0, void 0, function* () {
-            const response = yield fetch(url);
-            const data = yield response.json();
-            console.log(data);
-            const h1City = document.createElement("h1");
-            h1City.innerHTML = data.name;
-            document.body.appendChild(h1City);
-            const ul = document.createElement("ul");
-            const coordNow = document.createElement("li");
-            coordNow.innerHTML = "geolocation : " + lat + ", " + lon;
-            const liTemp = document.createElement("li");
-            liTemp.innerHTML = "current temperature: " + data.main.temp + " ºC";
-            const liClouds = document.createElement("li");
-            liClouds.innerHTML = "clouds : " + data.clouds.all + "%";
-            const liHum = document.createElement("li");
-            liHum.innerHTML = "humidity : " + data.main.humidity + "%";
-            const liWind = document.createElement("li");
-            liWind.innerHTML = "wind speed : " + data.wind.speed + " m/s";
-            ul.appendChild(coordNow);
-            ul.appendChild(liTemp);
-            ul.appendChild(liClouds);
-            ul.appendChild(liHum);
-            ul.appendChild(liWind);
-            document.body.appendChild(ul);
-        });
-        renderLocalList();
-    });
+  navigator.geolocation.getCurrentPosition(function (position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    console.log(lat, lon);
+    const url =
+      "https://api.openweathermap.org/data/2.5/weather?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&units=metric&appid=46079d357e9a2c65ff03fab0ab51f02c";
+    const renderLocalList = () =>
+      __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch(url);
+        const data = yield response.json();
+        console.log(data);
+        const h1City = document.createElement("h1");
+        h1City.innerHTML = data.name;
+        document.body.appendChild(h1City);
+        const ul = document.createElement("ul");
+        const coordNow = document.createElement("li");
+        coordNow.innerHTML = "geolocation : " + lat + ", " + lon;
+        const liTemp = document.createElement("li");
+        liTemp.innerHTML = "current temperature: " + data.main.temp + " ºC";
+        const liClouds = document.createElement("li");
+        liClouds.innerHTML = "clouds : " + data.clouds.all + "%";
+        const liHum = document.createElement("li");
+        liHum.innerHTML = "humidity : " + data.main.humidity + "%";
+        const liWind = document.createElement("li");
+        liWind.innerHTML = "wind speed : " + data.wind.speed + " m/s";
+        ul.appendChild(coordNow);
+        ul.appendChild(liTemp);
+        ul.appendChild(liClouds);
+        ul.appendChild(liHum);
+        ul.appendChild(liWind);
+        document.body.appendChild(ul);
+      });
+    renderLocalList();
+  });
 };
 getWeather();
-const getUSD = () => __awaiter(this, void 0, void 0, function* () {
-    const response = yield fetch("https://api.nbp.pl/api/exchangerates/rates/A/USD");
+const getUSD = () =>
+  __awaiter(this, void 0, void 0, function* () {
+    const response = yield fetch(
+      "https://api.nbp.pl/api/exchangerates/rates/A/USD"
+    );
     const data = yield response.json();
     let currency = data.code;
     let currVal = data.rates[0].mid;
@@ -55,10 +84,13 @@ const getUSD = () => __awaiter(this, void 0, void 0, function* () {
     usd.innerHTML = "Kurs " + currency + " : " + currVal + " PLN";
     document.body.appendChild(usd);
     console.log(data);
-});
+  });
 setTimeout(getUSD, 2000);
-const getEUR = () => __awaiter(this, void 0, void 0, function* () {
-    const response = yield fetch("https://api.nbp.pl/api/exchangerates/rates/A/EUR");
+const getEUR = () =>
+  __awaiter(this, void 0, void 0, function* () {
+    const response = yield fetch(
+      "https://api.nbp.pl/api/exchangerates/rates/A/EUR"
+    );
     const data = yield response.json();
     let currency = data.code;
     let currVal = data.rates[0].mid;
@@ -66,10 +98,13 @@ const getEUR = () => __awaiter(this, void 0, void 0, function* () {
     eur.innerHTML = "Kurs " + currency + " : " + currVal + " PLN";
     document.body.appendChild(eur);
     console.log(data);
-});
+  });
 setTimeout(getEUR, 3000);
-const getGBP = () => __awaiter(this, void 0, void 0, function* () {
-    const response = yield fetch("https://api.nbp.pl/api/exchangerates/rates/A/GBP");
+const getGBP = () =>
+  __awaiter(this, void 0, void 0, function* () {
+    const response = yield fetch(
+      "https://api.nbp.pl/api/exchangerates/rates/A/GBP"
+    );
     const data = yield response.json();
     let currency = data.code;
     let currVal = data.rates[0].mid;
@@ -77,9 +112,9 @@ const getGBP = () => __awaiter(this, void 0, void 0, function* () {
     gbp.innerHTML = "Kurs " + currency + " : " + currVal + " PLN";
     document.body.appendChild(gbp);
     console.log(data);
-});
+  });
 setTimeout(getGBP, 4000);
 function reload() {
-    document.location.reload();
+  document.location.reload();
 }
 setTimeout(reload, 15000);
